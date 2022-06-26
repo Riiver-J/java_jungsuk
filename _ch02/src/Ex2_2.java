15511a42eb8321a5650ef43c2fc2df2011207ac7
River J <ykjeong1223@naver.com>
2022-06-26 21:35:26 +0900

    boolean : can print TorF byte : range careful int oct : 0 int hex : 0x long l : ~1000_000_000 ~ over thing need < L > f : couldn't delete d : could delete

---
+++ _ch02/src/Ex2_2.java
@@ -0,0 +1,24 @@
+
+public class Ex2_2 {
+
+	public static void main(String[] args) {
+	final int score = 100;
+//	score = 200;
+	boolean power = true;
+	
+	byte b = 127; // -128~127
+	
+	int oct = 010; // 8진수,10진수로8
+	int hex = 0x10; // 16진수, 10진수로16
+	
+	long l = 10_000_000_000L;
+	
+	float f = 3.14f;
+	double d = 3.14;
+	System.out.println(10.);
+	System.out.println(.10);
+	System.out.println(10f);
+	System.out.println(1e3);
+	}
+
+}
