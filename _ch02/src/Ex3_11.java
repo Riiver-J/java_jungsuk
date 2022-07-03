4739fae9539f3b6879ed0f7a22101c568d5cf193
River J <ykjeong1223@naver.com>
2022-07-03 22:55:04 +0900

    if

--- _ch02/src/Ex3_11.java
+++ _ch02/src/Ex3_11.java
@@ -1,14 +1,12 @@
 class Ex3_11 {
 	public static void main(String args[]) { 
-		double pi = 3.141592; 
+		double pi = 3.141592; //3.141을 얻으려면?
 		
-		System.out.println(pi);
-		System.out.println(pi*1000);
-		System.out.println(Math.round(pi*1000)); //괄호주의
-		
-		System.out.println(Math.round(pi*1000)/1000); //3
-		System.out.println(Math.round(pi*1000)/1000.0); //3.142
-		System.out.println((double)Math.round(pi*1000)/1000); //3.142
+		System.out.println(pi*1000); //그리고형변환해서 값손실을내자
+		System.out.println((int)(pi*1000)); //()watch out
+		//실수가 정수로 바뀌면서 소수점 아래는 저장할 수 없으니 날라감
+		System.out.println((int)(pi*1000)/1000.0);
+		//1000으로 나누면 안돼 3.141로나와야되니까
 		
 //		double shortPi = Math.round(pi * 1000) / 1000.0;
 //		System.out.println(shortPi);
