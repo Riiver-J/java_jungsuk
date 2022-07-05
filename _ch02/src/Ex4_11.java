a08f4ad00b798f48b8a85b7129109afd9d3ee611
River J <ykjeong1223@naver.com>
2022-07-05 18:30:26 +0900

    중첩for문

---
+++ _ch02/src/Ex4_11.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Ex4_11 {
+	public static void main(String[] args) { 
+
+		//i=1,2,3,4,5 repeat 5 in{}
+//		for(int i=1;i<=5;i++) {
+//			System.out.println("**********");			
+//		}
+		
+//		for(int j=1;j<=10;j++) {
+//			System.out.print("*"); //without 'ln' 가로로출력
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=10;j++) {
+				System.out.print("*");
+			}
+			System.out.println(); //10개찍고줄바꿔
+		}
+		
+	} // main의끝
+	
+	//만약 j<=10을 j=i로 바꾸면 상수가 아니게되므로 i변화와 동일하게 변하면서 찍을수있
+}
---
+++ _ch02/src/Ex4_8.java
@@ -0,0 +1,7 @@
+class Ex4_8 {
+	public static void main(String args[]) {
+		for (int i = 1; i <= 10; i=i++) { // {}안의 문장을 5번 반복,1부터 10까지 1씩증
+			System.out.println(i);
+		}
+	}
+}
