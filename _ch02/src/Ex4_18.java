1ebd5f124357f17a12948446053fd9fbd3e9563d
River J <ykjeong1223@naver.com>
2022-07-06 23:47:51 +0900

    break

--- _ch02/src/Ex4_18.java
+++ _ch02/src/Ex4_18.java
@@ -7,14 +7,14 @@ class Ex4_18 {
 
 		Scanner scanner = new Scanner(System.in);
 
-		while(true) {
+		while(true) { //무한반복문
 			System.out.println("(1) square");
 			System.out.println("(2) square root");
 			System.out.println("(3) log");
 			System.out.print("원하는 메뉴(1~3)를 선택하세요.(종료:0)>");
 
-			String tmp = scanner.nextLine(); // ȭ�鿡�� �Է¹��� ������ tmp�� ����
-			menu = Integer.parseInt(tmp);    // �Է¹��� ���ڿ�(tmp)�� ���ڷ� ��ȯ
+			String tmp = scanner.nextLine(); // 화면에서 입력받은 내용을 tmp에 저장
+			menu = Integer.parseInt(tmp);    // 입력받은 문자열(tmp)을 숫자로 변환
 
 			if(menu==0) {
 				System.out.println("프로그램을 종료합니다.");
