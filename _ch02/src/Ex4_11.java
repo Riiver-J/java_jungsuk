5c4dd7f838bc266033784bd05b8379e54d1be4fd
River J <ykjeong1223@naver.com>
2022-07-20 16:25:32 +0900

    별찍기 복습.

--- _ch02/src/Ex4_11.java
+++ _ch02/src/Ex4_11.java
@@ -13,12 +13,12 @@ class Ex4_11 {
 		
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=10;j++) {
-				System.out.print("*");
+				System.out.print("**");
 			}
 			System.out.println(); //10개찍고줄바꿔
 		}
 		
-	} // main의끝
+	} // main의끝.
 	
 	//만약 j<=10을 j=i로 바꾸면 상수가 아니게되므로 i변화와 동일하게 변하면서 찍을수있
 }
