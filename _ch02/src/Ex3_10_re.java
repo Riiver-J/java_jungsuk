34b2e29a258c109a2ffb15d2daf6b14131bee06c
River J <ykjeong1223@naver.com>
2022-08-02 21:55:00 +0900

    overflow

---
+++ _ch02/src/Ex3_10_re.java
@@ -0,0 +1,13 @@
+class Ex3_10_re {
+	public static void main(String args[]) { 
+		int a = 1_000_000;    // 1,000,000   1백만=10의6제곱
+		int b = 2_000_000;    // 2,000,000   2백만=10의6제곱
+		
+		//10의 12제곱이나와야되는데  오버플로우발생
+		//int의 범위는 10의9제곱이니까 a,b둘중하나를 형변환시켜야됨
+
+		long c = (long)a * b;       // a * b = 2,000,000,000,000 ?
+
+		System.out.println(c);
+	}
+}
