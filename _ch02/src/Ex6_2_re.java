822c77b8501bfa269b9b6c3c5238949aecdf09d1
Riiver-J <ykjeong1223@naver.com>
2022-08-11 23:09:58 +0900

    객체 티비 복

---
+++ _ch02/src/Ex6_2_re.java
@@ -0,0 +1,15 @@
+class Ex6_2_re {
+	public static void main(String args[]) {
+		Tv t1 = new Tv();  // Tv t1; t1 = new Tv();�� �� �������� ����
+		Tv t2 = new Tv();
+		System.out.println("t1�� channel���� " + t1.channel + "�Դϴ�.");
+		System.out.println("t2�� channel���� " + t2.channel + "�Դϴ�.");
+
+		t1.channel = 7;    // channel ���� 7���� �Ѵ�.
+		System.out.println("t1�� channel���� 7�� �����Ͽ����ϴ�.");
+
+		t2 = t1; //참조변수 t1값을 t2에 저
+		System.out.println("t1�� channel���� " + t1.channel + "�Դϴ�.");
+		System.out.println("t2�� channel���� " + t2.channel + "�Դϴ�.");
+	}
+}
