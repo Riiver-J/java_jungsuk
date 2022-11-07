c52d54e41d77e9770170f17a507d746c812ece54
Riiver-J <ykjeong1223@naver.com>
2022-11-07 23:30:34 +0900

    Implement of Thread

--- _ch02/src/Ex13_1.java
+++ _ch02/src/Ex13_1.java
@@ -10,19 +10,19 @@ class Ex13_1 {
 	}
 }
 
-class ThreadEx1_1 extends Thread {
-	public void run() {
+class ThreadEx1_1 extends Thread {	// Thread클래스를 상속하여 쓰레드를 구현하는 방식
+	public void run() {	// 쓰레드가 수행할 작업을 작성 ==main 
 		for(int i=0; i < 5; i++) {
-			System.out.println(getName()); 
+			System.out.println(this.getName()); // 조상인 Thread의 getName()을 호출하므로 this.생략 가능
 		}
 	}
 }
 
-class ThreadEx1_2 implements Runnable {
-	public void run() {
-		for(int i=0; i < 5; i++) {
+class ThreadEx1_2 implements Runnable {	// Runnable클래스를 구현하여 쓰레드를 구현하는 방식
+	public void run() {	// 쓰레드가 수행할 작업을 작성
+		for(int i=0; i < 5; i++) {	 
 			
-			System.out.println(Thread.currentThread().getName());
+			System.out.println(Thread.currentThread().getName()); // Thread.currentTread().:현재실행중인 Thread를 반환. 인터페이스라 참조해야
 		}
 	}
 }
